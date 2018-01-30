52a242d0103f1116e53bbacd1911bdc6d9e4a1d0
shakeel999 <shakeel999sourceus@gmail.com>
2018-01-30 12:38:47 -0800

    some files added 30-01-2018

--- loginpage.php
+++ loginpage.php
@@ -20,32 +20,32 @@
 	
 	  <div class="container-fluid">
 <div class="row">
-    <div class="col-md-4">
+    <div class="col-md-3">
     </div>
 
-    <div class="col-md-4" id="sab-login1">
+    <div class="col-md-6" id="sab-login1">
 
 <div class="row" >
-  <div class="col-md-5" style="background-color:#2874F0; ">
+  <div class="col-md-4" style="background-color:#2874F0; ">
     <h2 class="mt-3 ml-3 text-white">Login</h2>
     <p class="ml-3 text-white text-justify">
       Lorem Ipsum is simply dummy text of the printing and typesetting 
     </p>
-    <img src="images/job2.png" class="img-fluid">
+    <img src="images/job2.png" class="img-fluid" style="margin-top:50%; ">
  
   </div>
-  <div class="col-md-7">
+  <div class="col-md-8 " style="height:620px;">
 <form class="mt-3 ml-3">
       <div class="form-group sab-form-group2">
-
+            <label for="email" >Email:</label>
          <input type="E-mail" class="form-control sab-form-control" id="email" placeholder="E-mail id" required=""  >
       </div>
              
       <div class="form-group sab-form-group2">
-      
+        <label for="email" >Passwords:</label>
          <input type="password" class="form-control sab-form-control"  id="password" placeholder="password" name="password" required="" >
      </div>
-     <a href="smc.php" class="btn btn-lg btn-primary btn-block btn-sm sab-btn2">Login</a>
+     <a href="smc.php" class="btn btn-lg btn-primary btn-block sab-btn2">Login</a>
           <center><a href="#"><p class="sab-bottom-text2">Forgotton your password?</p></a></center>
           <center><a href="Sellform.php"><p class="sab-bottom-text2">Create an account</p></a></center>
 
@@ -53,31 +53,129 @@
   </form>
 
   </div>
-</div>
-
-
-
-
-
-
-
 </div>  
-      <div class="col-md-4">
-      </div>
-
-
-
-   
-   </div>
-   </div>                                          
+</div>  
+      <div class="col-md-3">
+      </div>  
+  </div>
+  </div>                                          
 		
 		</div>
 
 		
 		
 		
+
+
+
+
+
+
+
+    <!-- signup form -->
 		
-		
+		<div class="container-fluid">
+<div class="row">
+    <div class="col-md-3">
+    </div>
+
+
+    <div class="col-md-6 mt-5" id="sab-signup" "> 
+
+
+<div class="row">
+  <div class="col-md-4" style="background-color:#2874F0; ">
+    <h2 class="mt-3 ml-3 text-white">Sign Up</h2>
+    <p class="ml-3 text-white text-justify">
+     
+      
+      <ul>
+        <h5>Stand out</h5>
+<hr>
+       <h5>Post Jobs for Free</h5>
+    
+<hr>
+
+ <h5>Engage with Talent</h5>
+    
+<hr>
+
+     </ul> </p>
+    
+    <img src="images/signup4.png" class="img-fluid" style="margin-top:100px;-webkit-filter: drop-shadow(10px 10px 10px #222);
+  filter: drop-shadow(15px 15px 15px #222);  ">
+ 
+  </div>
+
+
+  <div class="col-md-8 p-3" style="height:620px;" >
+<form>
+      <div class="form-group ">
+            <label for="email" >Name :</label>
+         <input type="name" class="form-control" id="name" placeholder="Enter your full name" required=""  >
+      </div>
+             
+    
+     <div class="form-group">
+        <label for="email" >Email :</label>
+         <input type="email" class="form-control"  id="email" placeholder="Enter your active email id to recieve relevant jobs" name="Email" required="" >
+     </div>
+       <div class="form-group ">
+        <label for="email" >Password :</label>
+         <input type="password" class="form-control"  id="password" placeholder="password" name="password" required="" >
+     </div>
+      <div class="form-group ">
+        <label for="email" >Confirm password :</label>
+         <input type="password" class="form-control"  id="password" placeholder="Confirm password" name="confirm password" required="" >
+     </div>
+   <div class="form-group ">
+        <label for="tel" >mobile number:</label>
+         <input type="tel" class="form-control"  id="PN" placeholder="Enter your mobile number" name="mobile" required="" >
+     </div>
+     <label for="tel" >Location:</label>
+        <input list="browsers" class="form-control" placeholder="choose your location" name="browser" class="w-100">
+  <datalist id="browsers">
+    <option value="bangalore"></option>
+    <option value="pune"></option>
+    <option value="Chrome"></option>
+    <option value="Opera"></option>
+    <option value="Safari"></option>
+  </datalist>
+
+<br>
+     <a href="smc.php" class="btn btn-lg btn-primary btn-block  sab-btn2">Sign In</a>
+         
+         <p class="text-right mt-1">already a user <a href="#">Sign In</a></p>
+
+
+  </form>
+  </div>
+</div>
+  
+
+
+
+
+
+
+
+
+
+
+</div>
+
+      <div class="col-md-3">
+      </div>
+
+
+
+  
+   </div>
+   </div>                                          
+    
+    </div>
+
+    <br><br>
 		
 		
 		
--- signup.php
+++ signup.php
@@ -13,89 +13,46 @@
   <!--navbar-->
   <?php include_once "header.php"; ?>
   <!--end of navbar-->
-        
-<div class="container-fluid m-0 p-0">
-  <div class="row"> 
-
+      <div class="container-fluid m-0 p-0">
+	<div class="row">	
   
-    <div class="container-fluid">
-<div class="row">
+
+
+
+  <div class="container-fluid">
+   <div class="row">
     <div class="col-md-3">
     </div>
-
-
-    <div class="col-md-6 mt-5" id="sab-signup"> 
-
-
-<div class="row">
-  <div class="col-md-4" style="background-color:#2874F0; ">
-    <h2 class="mt-3 ml-3 text-white">Sign Up</h2>
+      <div class="col-md-6" id="sab-login1">
+      <div class="row">
+      <div class="col-md-4" style="background-color:#2874F0; ">
+            <h2 class="mt-3 ml-3 text-white">Login</h2>
     <p class="ml-3 text-white text-justify">
-     
-      
-      <ul>
-        <h5>Stand out</h5>
-<hr>
-       <h5>Post Jobs for Free</h5>
-    
-<hr>
-
- <h5>Engage with Talent</h5>
-    
-<hr>
-
-     </ul> </p>
-    
-    <img src="images/signup4.png" class="img-fluid" style="margin-top:100px;-webkit-filter: drop-shadow(10px 10px 10px #222);
-  filter: drop-shadow(15px 15px 15px #222);  ">
+      Lorem Ipsum is simply dummy text of the printing and typesetting 
+    </p>
+     <img src="images/job2.png" class="img-fluid" style=" margin-top:50%; ">
  
   </div>
-
-
-  <div class="col-md-8 p-4" >
-<form>
-      <div class="form-group ">
-            <label for="email" >Name :</label>
-         <input type="name" class="form-control" id="name" placeholder="Enter your full name" required=""  >
+     <div class="col-md-8" style="height:620px; ">
+  <form class="mt-3 ml-3">
+      <div class="form-group sab-form-group2">
+            <label for="email" >Email:</label>
+         <input type="E-mail" class="form-control sab-form-control" id="email" placeholder="E-mail id" required=""  >
       </div>
              
-    
-     <div class="form-group">
-        <label for="email" >Email :</label>
-         <input type="email" class="form-control"  id="email" placeholder="Enter your active email id to recieve relevant jobs" name="Email" required="" >
+      <div class="form-group sab-form-group2">
+       <label for="email" >Password :</label>
+         <input type="password" class="form-control sab-form-control"  id="password" placeholder="password" name="password" required="" >
      </div>
-       <div class="form-group ">
-        <label for="email" >Password :</label>
-         <input type="password" class="form-control"  id="password" placeholder="password" name="password" required="" >
-     </div>
-      <div class="form-group ">
-        <label for="email" >Confirm password :</label>
-         <input type="password" class="form-control"  id="password" placeholder="Confirm password" name="confirm password" required="" >
-     </div>
-   <div class="form-group ">
-        <label for="tel" >mobile number:</label>
-         <input type="tel" class="form-control"  id="PN" placeholder="Enter your mobile number" name="mobile" required="" >
-     </div>
-     <label for="tel" >Location:</label>
-        <input list="browsers" class="form-control" placeholder="choose your location" name="browser" class="w-100">
-  <datalist id="browsers">
-    <option value="bangalore"></option>
-    <option value="pune"></option>
-    <option value="Chrome"></option>
-    <option value="Opera"></option>
-    <option value="Safari"></option>
-  </datalist>
-
-<br>
-     <a href="smc.php" class="btn btn-lg btn-primary btn-block  sab-btn2">Sign In</a>
-         
-         <p class="text-right mt-1">already a user <a href="#">Sign In</a></p>
+     <a href="smc.php" class="btn btn-lg btn-primary btn-block sab-btn2">Login</a>
+          <center><a href="#"><p class="sab-bottom-text2">Forgotton your password?</p></a></center>
+          <center><a href="Sellform.php"><p class="sab-bottom-text2">Create an account</p></a></center>
 
 
   </form>
+
   </div>
-</div>
-  
+</div>  
 
 
 
@@ -103,31 +60,31 @@
 
 
 
-
-
-
-</div>
-
+</div>  
       <div class="col-md-3">
       </div>
 
 
 
-  
+   
    </div>
    </div>                                          
-    
-    </div>
+		
+		</div>
 
-    
-    
-    
-    
+	</div>
     
     
     
     
     <br><br>
+
+
+
+
+
+
+
 
 
 
@@ -161,12 +118,12 @@
      </ul> </p>
     
     <img src="images/signup1.png" class="img-fluid" style="margin-top:100px; -webkit-filter: drop-shadow(10px 10px 10px #ccc);
-  filter: drop-shadow(5px 5px 5px #ccc); ">
+  filter: drop-shadow(5px 5px 5px #ccc);">
  
   </div>
 
 
-  <div class="col-md-8 p-4" >
+  <div class="col-md-8 p-4 " style="height:630px; " >
 <form>
       <div class="form-group ">
             <label for="email" >Name :</label>
@@ -231,15 +188,17 @@
    </div>
    </div>                                          
     
-    </div>
-    <br><br>                 
- 
-            
+
+    <br><br>      
+
+
+
+
           
   <!--footer-->
   <?php include_once "footer.php"; ?>
   <!-- end of footer-->    
-  </div>
+ </div>
   <?php include_once "script.php"; ?>
   </body>
 </html>
