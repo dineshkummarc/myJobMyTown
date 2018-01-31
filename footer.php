fb37eae7d2df3843cb61baeceaf47831d64a54b7
sumaiyakounain <sumaiyakounainsourceus@gmail.com>
2018-01-31 17:41:51 +0530

    created footer

---
+++ footer.php
@@ -0,0 +1,67 @@
+
+<footer class="footer" style="background-color: #E0DEDC;">
+
+      <div class="container">
+  <div class="row anchor" style="padding-top: 10px;">
+    <div class="col-6 col-sm-3">
+      <ul><h6>Information</h6>
+        <li>
+          <a href="aboutus.php"> About Us</a>
+        </li>
+        <li>
+          <a href="#">Terms and Conditions</a>
+        </li>
+        <li>
+         <a href="ourteam.php">Privacy Policy</a>
+        </li>
+        <li>
+         <a href="faq.php">Careers with Us</a>
+        </li>
+        <li>
+          <a href="contactus.php">FAQs</a>
+        </li>
+        <li>
+          <a href="contactus.php"> Contact Us </a>
+        </li>
+      </ul>
+    </div>
+    <div class="col-6 col-sm-3">
+    <ul> <h6>Job Seekers</h6>
+        <li>
+          <a href="blog.php">Register now</a>
+        </li>
+        <li>
+          <a href="#"> Search jobs</a>
+        </li>
+        <li>
+          <a href="#">Build Resume</a>
+        </li>
+        <li>
+          <a href="#">Create Job alert</a>
+        </li>
+        <li>
+          <a href="#">Blogs</a>
+        </li>
+      </ul>
+    </div>
+    <div class="col-6 col-sm-3">
+<ul> <h6>Employers</h6>
+        <li>
+          <a href="editorial.php">Post jobs</a>
+        </li>
+        <li>
+          <a href="sellphotos.php">Recuiters</a>
+        </li>    
+      </ul>
+    </div>
+        <div class="col-6 col-sm-3"> <h6>Follow Us</h6>
+       <a href="http://facebook.com/"><i class="fa fa-facebook-official fa-2x footericon" aria-hidden="true"></i></a>
+        <a href="http://twitter.com/"><i class="fa fa-twitter-square fa-2x footericon" aria-hidden="true"></i></a>
+        <a href="#/"><i class="fa fa-google-plus-square fa-2x footericon" aria-hidden="true"></i></a>
+    </div>
+  </div>
+
+</div>
+<hr color="White">
+<center><p style="padding-bottom: 15px;"><font color="">&copy;All rights Reserved by : <a href="http://sourceuscorporation.com/"><b>SUCO TECHNOLOGIES.</b></a></font></p></center>
+</footer>
